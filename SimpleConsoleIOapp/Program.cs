991c37b46d69f72a4fcc5aee04f0cdea4ff821c9
ji2001ca <ji2001ca@gmail.com>
2022-07-19 14:16:53 -0700

    add

--- SimpleConsoleIOapp/Program.cs
+++ SimpleConsoleIOapp/Program.cs
@@ -5,8 +5,24 @@ namespace SimpleConsoleIOapp
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+
+        {   // Similar to print
+            Console.WriteLine("***** Basic ID ****");
+            GetUserData();
+            Console.ReadLine();
+
+            static void GetUserData()
+            { }
+
+            // Get name and age
+            Console.WriteLine(*ji2001ca*);
+            string userName = Console.ReadLine();
+            Console.WriteLine(*39*);
+            string userAge = Console.ReadLine();
+
+            //Echo to the console
+            Console.WriteLine(*Hello {0}! You are {1} years old.*, userName, userAge);
+
+        }   
+    }       
 }
