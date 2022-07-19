f6b3e2ea0d343e50327fe824d0db51e1a1fbba56
Gflocb <gflocb@gmail.com>
2022-07-19 12:40:44 -0700

    Finally, our fast commit

--- SimpleConsoleIOapp/Program.cs
+++ SimpleConsoleIOapp/Program.cs
@@ -6,7 +6,24 @@ namespace SimpleConsoleIOapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("**** Basic IO ****");
+            GetUserData();
+            Console.WriteLine();
+
+            //Create a feature for awaiting user input before terminating
+            Console.ReadLine();
+
+            static void GetUserData()
+            {
+                //Getname and age
+                Console.Write("Enter your name: ");
+                string userName = Console.ReadLine();
+                Console.Write("Enter your age: ");
+                string userAge = Console.ReadLine();
+
+                //Echo to the console
+                Console.WriteLine("Hello {0}! You are {1} years old.", userName, userAge);
+            }
         }
     }
 }
