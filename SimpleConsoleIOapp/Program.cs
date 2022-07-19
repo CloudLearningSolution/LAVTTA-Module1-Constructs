3aa1681fdc5b1186c5ef2867c0a0305990f5bd1c
nexushawkins <mariebnexus@gmail.com>
2022-07-19 14:42:49 -0700

    push changes

--- SimpleConsoleIOapp/Program.cs
+++ SimpleConsoleIOapp/Program.cs
@@ -6,22 +6,33 @@ namespace SimpleConsoleIOapp
     {
         static void Main(string[] args)
         {
-          //Similar to print
+            //Similar to print
             Console.WriteLine("****Basic I0****");
             GetUserData();
-           Console.ReadLine();
+            Console.ReadLine();
 
-        static void GetUserData()
-        {
+            static void GetUserData()
+            {
                 // Get name and age 
                 Console.Write(" Enter your name:");
                 String UserName = Console.ReadLine();
-            Console.WriteLine("enter your age:");
-            String userAge = Console.ReadLine();
+                Console.WriteLine("enter your age:");
+                string userAge = Console.ReadLine();
 
                 //Echo to the console
-                Console.WriteLine("Hello {0}! You are  {1} years old.", UserName, userAge);
+                Console.WriteLine("Hello {0}! You are {1} years old.", UserName, userAge);
+            }
 
-     
-        
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
 
