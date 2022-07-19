67cafd208325f569b38cc3ad7cd7f59ac4b583c9
RebelBobby <88352541+RebelBobby@users.noreply.github.com>
2022-07-19 13:30:56 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Joseph Matulewicz
+
+using System;
+
+namespace SimpleConsoleIOapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Basic IO ****");
+            GetUserData();
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+            static void GetUserData()
+            {
+                Console.Write("Enter your name: ");
+                string userName = Console.ReadLine();
+                Console.Write("Enter your age: ");
+                string userAge = Console.ReadLine();
+
+                Console.WriteLine("Hello {0}! You are {1} years old.", userName, userAge);
+            }
+        }
+    }
+}
+
