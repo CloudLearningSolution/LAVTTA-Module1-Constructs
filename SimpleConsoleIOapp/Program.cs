269ed13e1143a7eee80fd1a501306cc6a0a9929e
alfonso <alfonso.studio151@gmail.com>
2022-07-19 13:16:04 -0700

    feature added

--- SimpleConsoleIOapp/Program.cs
+++ SimpleConsoleIOapp/Program.cs
@@ -6,7 +6,22 @@ namespace SimpleConsoleIOapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Basic IO");
+            GetUserData();
+            Console.ReadLine();
+
+            static void GetUserData()
+            {
+                //Get name
+                Console.Write("Enter your name: ");
+                string userName = Console.ReadLine();
+                Console.Write("Enter your age: ");
+                string userAge = Console.ReadLine();
+
+                //Echo to console
+                Console.WriteLine("Hello {0} You are {1} years old.", userName, userAge);
+
+            }
         }
     }
 }
