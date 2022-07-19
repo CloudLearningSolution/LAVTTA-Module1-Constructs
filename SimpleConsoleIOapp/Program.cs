bf9422d8b8a914e57c4ec3a88ace37b0445dc68f
beardface87 <bryon.hawk@gmail.com>
2022-07-19 13:26:37 -0700

    add

--- SimpleConsoleIOapp/Program.cs
+++ SimpleConsoleIOapp/Program.cs
@@ -6,7 +6,25 @@ namespace SimpleConsoleIOapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Similar to print
+         Console.WriteLine("**** Basic****");
+         GetUserData();
+         Console.ReadLine();
+
+         static void GetUserData()
+          {
+                // Get name and age
+                Console.WriteLine("Enter your name: ");
+                string username = Console.ReadLine();
+                Console.Write("Enter your age:");
+                string userage = Console.ReadLine();
+
+                //Echo to the console
+            Console.WriteLine("Hello {0}! You are {1} years old.", username, userage);
+            }
         }
-    }
-}
+   
+     
+
+
+       
