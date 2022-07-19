574a41a3d46f89543d9b0a11e6628ca85d574768
DABDAB1987-Fastlaneus <dinobaptiste@icloud.com>
2022-07-19 13:54:40 -0700

    add

--- SimpleConsoleIOapp/Program.cs
+++ SimpleConsoleIOapp/Program.cs
@@ -3,10 +3,20 @@
 namespace SimpleConsoleIOapp
 {
     class Program
-    {
+    {;
+        //simiiar to print
+        Console.Writline("*********Basic I/O**********);
+        String userName = Console.ReadLine();
+        Console.Writeline("Enter your age:  ");
+        String userAge = Console.ReadLine();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            //Create a feaature for awaiting user input before teminiation
+            Console.ReadLine();
         }
     }
 }
