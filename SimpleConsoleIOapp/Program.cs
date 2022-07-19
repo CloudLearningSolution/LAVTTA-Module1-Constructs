6bea5a1dff856d13101e64988a827f8df51da98f
wakingDreams317 <jody.roane@gmail.com>
2022-07-19 12:40:48 -0700

    Added get user data feature.

--- SimpleConsoleIOapp/Program.cs
+++ SimpleConsoleIOapp/Program.cs
@@ -4,9 +4,27 @@ namespace SimpleConsoleIOapp
 {
     class Program
     {
+        // Create Main() method.
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        {// Begining of Main() method.
+            Console.WriteLine("**** Basic IO ****");
+
+            // Call GetUserData() method.
+            GetUserData();
+            Console.ReadLine();
+
+            // Define GetUserData() method.
+            static void GetUserData()
+            {// Begining of GetUserData() method.
+                // Get name and age.
+                Console.Write("What is your name: ");
+                string userName = Console.ReadLine();
+                Console.Write("What is your age: ");
+                string userAge = Console.ReadLine();
+
+                //Echo to the Console.
+                Console.WriteLine("Hello {0}! You are {1} years old." , userName , userAge);
+            }// End of GetUserData() method.
+        }// End of Main() method.
     }
 }
