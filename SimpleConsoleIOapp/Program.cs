84965ce2a841e70f675637020077ac530c314060
bulletproofazn <bulletproofazn@icloud.com>
2022-07-19 13:12:00 -0700

    added get userName and userAge, played with basic IO

--- SimpleConsoleIOapp/Program.cs
+++ SimpleConsoleIOapp/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
 namespace SimpleConsoleIOapp
-{
+{ 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("**** Basic IO ****");
+            GetUserData();
+            Console.ReadLine();
+
+            static void GetUserData()
+            {
+                // Get name and age
+                Console.Write("Enter your name: ");
+                string userName = Console.ReadLine();
+                Console.Write("Enter your age: ");
+                string userAge = Console.ReadLine();
+
+                //Echo to the console
+                Console.WriteLine("Hello {0}! You are {1} years old.", userName, userAge);
+            }
         }
     }
 }
