b90425159922ccd0fb850e2d4101cf390ce8f322
Dwells22-fastlaneus <donnie.wells@fastlaneus.com>
2022-07-19 14:11:46 -0700

    add

--- SimpleConsoleIOapp/Program.cs
+++ SimpleConsoleIOapp/Program.cs
@@ -6,7 +6,24 @@ namespace SimpleConsoleIOapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Similar to print
+            Console.WriteLine("**** Basic IO ****");
+            GetUserData();
+            Console.ReadLine();
+
+            static void GetUserData()
+            {
+                // Get name and age
+                Console.Write("Enter your name: ");
+                string userName = Console.ReadLine();
+                Console.Write("Enter your age: ");
+                string userAge = Console.ReadLine();
+
+                //Echo to the console
+                Console.WriteLine("Hello {0}! You are {1} years old.", userName, userAge);
+            }
         }
+
     }
+
 }
